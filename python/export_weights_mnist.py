8e1f838a2b415d3b16ca81ae2959d0a462899386
Mariana <marianahines@hotmail.com>
2026-04-01 21:19:06 -0300

    update weights

--- python/export_weights_mnist.py
+++ python/export_weights_mnist.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
